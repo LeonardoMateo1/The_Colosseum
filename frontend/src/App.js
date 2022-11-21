57cd66811496800747ac6447ae519b9ee8b58b54
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2022-11-21 08:49:20 -0500

    browser router / component / images setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from './components/Home_Page/Home';
+import SignUp from './components/signup/SignUp';
+import Login from './components/login/Login'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Sign_Up" element={<SignUp/>}/>
+          <Route path='/Login' element={<Login/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
