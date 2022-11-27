142e4c2013f5a7a3673031558115099f6fd18f1b
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2022-11-26 23:49:01 -0500

    Finished nav design

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from '../../images/logo2.png'
 import "./home.css"
 import {Link} from "react-router-dom"
+import {AiOutlineSearch} from "react-icons/ai"
 
 
 const Home = () => {
@@ -9,13 +10,21 @@ const Home = () => {
     <div>
       <div className="home_header">
         <img className='img' src={Logo} alt="" />
-        <h3>Welcome to the Colosseum</h3>
-        <div className="sig_log">
-          <div className="log_left">
-            <Link className='link' to="/Login">Login /</Link>
+        <div className="header_right">
+          <Link className='link' to=''>Players</Link>
+          <Link className='link' to=''>Ranknings</Link>
+          <Link className='link' to=''>Tournaments</Link>
+          <div className="search_bar">
+            <a href="#"><AiOutlineSearch/></a>
+            <input type="search" id='search'/>
           </div>
-          <div className="sig_right">
-            <Link className='link' to="/Sign_Up">Sign Up</Link>
+          <div className="sig_log">
+            <div className="log_left">
+              <Link className='link' to="/Login">Login /</Link>
+            </div>
+            <div className="sig_right">
+              <Link className='link' to="/Sign_Up">Sign Up</Link>
+            </div>
           </div>
         </div>
       </div>
